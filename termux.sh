94906c7045c8c46a3c75dc0ae3a8c2237871cab6
Paulochosen <pauloibrsilva@gmail.com>
2025-12-05 21:46:42 -0300

    Criar termux.sh

---
+++ termux.sh
@@ -0,0 +1,37 @@
+Report issues at https://bugs.termux.com
+┌──(OlhoDeDeus㉿u0_a606)-[~]
+└─$ pkg update -y && pkg upgrade -y
+pkg install nmap git python -y
+Hit:1 https://root-packages.termux.net root InRelease
+Hit:2 https://x11-packages.termux.net x11 InRelease
+Hit:3 https://termux.net stable InRelease
+All packages are up to date.
+Hit:1 https://root-packages.termux.net root InRelease
+Hit:2 https://x11-packages.termux.net x11 InRelease
+Hit:3 https://termux.net stable InRelease
+All packages are up to date.
+Summary:
+  Upgrading: 0, Installing: 0, Removing: 0, Not Upgrading: 0
+nmap is already the newest version (7.98).
+python is already the newest version (3.12.12).
+python set to manually installed.
+Installing:
+  git
+
+Suggested packages:
+  perl
+
+Summary:
+  Upgrading: 0, Installing: 1, Removing: 0, Not Upgrading: 0
+  Download size: 4984 kB
+  Space needed: 30.4 MB
+
+Get:1 https://termux.net stable/main aarch64 git aarch64 2.52.0 [4984 kB]
+Fetched 4984 kB in 9s (535 kB/s)
+Selecting previously unselected package git.
+(Reading database ... 19416 files and directories currently installed.)
+Preparing to unpack .../git_2.52.0_aarch64.deb ...
+Unpacking git (2.52.0) ...
+Setting up git (2.52.0) ...
+┌──(OlhoDeDeus㉿u0_a606)-[~]
+└─$
